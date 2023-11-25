72d4a379156d5c9f9e3c2b3d1c7ce92cd919130d
Ganesh Bista <117885646+ganeshbistakaji@users.noreply.github.com>
2023-11-25 20:43:49 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,9 @@ audio.loop = true;
 
 var waveElement = document.getElementById('wave');
 
-waveElement.addEventListener('mouseover', function() {
-    if (audio.paused) {
-        audio.play();
-    }
+waveElement.addEventListener('click', function() {
+    audio.play();
 
     document.getElementById('body').style.display = 'none';
     document.getElementById('sus').style.display = 'block';
-});
-
-document.addEventListener('click', function() {
-    audio.pause();
 });
