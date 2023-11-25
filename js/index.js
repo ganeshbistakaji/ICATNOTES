ca2900b77300409493e6f428f7fbde3764e225a9
Ganesh Bista <117885646+ganeshbistakaji@users.noreply.github.com>
2023-11-25 22:28:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-
-var audio = new Audio('../audio/sus.mp3'); 
-audio.loop = true;
-
 var waveElement = document.getElementById('wave');
 
 waveElement.addEventListener('click', function() {
-    if (audio.paused) {
-        audio.play();
-    }
+    play();
 
     document.getElementById('body').style.display = 'none';
     document.getElementById('sus').style.display = 'block';
 });
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.loop = true;
+    audio.play();
+}
