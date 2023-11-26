a183b6321a8a46cc25e40afedbdabd6f960a1b2c
Ganesh Bista <117885646+ganeshbistakaji@users.noreply.github.com>
2023-11-26 20:36:30 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+
+// var audio = new Audio('../audio/sus.mp3'); 
+// audio.loop = true;
+
+// var waveElement = document.getElementById('wave');
+
+// waveElement.addEventListener('click', function() {
+//     if (audio.paused) {
+//         audio.play();
+//     }
+
+//     document.getElementById('body').style.display = 'none';
+//     document.getElementById('sus').style.display = 'block';
+// });
+
+
+// var audio = new Audio('../audio/sus.mp3'); 
+// audio.loop = true;
+
 var waveElement = document.getElementById('wave');
 
 waveElement.addEventListener('click', function() {
@@ -6,6 +25,8 @@ waveElement.addEventListener('click', function() {
     document.getElementById('body').style.display = 'none';
     document.getElementById('sus').style.display = 'block';
 });
+
+
 
 function play() {
     var audio = document.getElementById("audio");
